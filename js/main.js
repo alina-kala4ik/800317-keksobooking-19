28becd72dd26a460531e746a02a54186658fca64
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-01-30 13:25:56 +0300

    правки по замечаниям

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@
 
   document.querySelector('.map').classList.remove('map--faded');
 
-  var ads = [{}, {}, {}, {}, {}, {}, {}, {}];
+  var ads = [];
 
-  var types = ['palace', 'flat', 'house', 'bungalo'];
+  var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 
-  var checkTimes = ['12:00', '13:00', '14:00'];
+  var CHECK_TIMES = ['12:00', '13:00', '14:00'];
 
-  var allFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var ALL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
   var features = [];
 
-  var allPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  var ALL_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
   var photos = [];
 
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -28,40 +28,42 @@
 
   var generateFeatures = function () {
     for (var i = 0; i < randomInteger(0, 5); i++) {
-      features[i] = allFeatures[i];
+      features[i] = ALL_FEATURES[i];
     }
     return features;
   };
 
   var generatePhotos = function () {
     for (var i = 0; i < randomInteger(0, 2); i++) {
-      photos[i] = allPhotos[i];
+      photos[i] = ALL_PHOTOS[i];
     }
     return photos;
   };
 
-  var generateAds = function (ad, i) {
-    ad.author = {
-      'avatar': 'img/avatars/user0' + (i + 1) + '.png'
-    };
-    var checkTime = checkTimes[randomInteger(0, 2)];
-    ad.offer = {
-      'title': 'описание',
-      'address': '600, 350',
-      'price': randomInteger(30, 500),
-      'type': types[randomInteger(0, 3)],
-      'rooms': randomInteger(1, 8),
-      'guests': randomInteger(1, 5),
-      'checkin': checkTime,
-      'checkout': checkTime,
-      'features': generateFeatures(),
-      'description': 'описание',
-      'photos': generatePhotos()
-    };
-    ad.location = {
-      'x': randomInteger(0, 1200),
-      'y': randomInteger(130, 630)
-    };
+  var generateAds = function (i) {
+    var checkTime = CHECK_TIMES[randomInteger(0, 2)];
+    ads[i] = {
+      'author': {
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      'offer': {
+        'title': 'описание',
+        'address': '600, 350',
+        'price': randomInteger(30, 500),
+        'type': TYPES[randomInteger(0, 3)],
+        'rooms': randomInteger(1, 8),
+        'guests': randomInteger(1, 5),
+        'checkin': checkTime,
+        'checkout': checkTime,
+        'features': generateFeatures(),
+        'description': 'описание',
+        'photos': generatePhotos()
+      },
+      'location': {
+        'x': randomInteger(0, 1200),
+        'y': randomInteger(130, 630)
+      }
+    }
   };
 
   var generatePinElement = function (ad) {
@@ -74,7 +76,7 @@
   };
 
   for (var i = 0; i < 8; i++) {
-    generateAds(ads[i], i);
+    generateAds(i);
     generatePinElement(ads[i]);
   }
 
