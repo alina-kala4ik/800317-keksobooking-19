fe72e6c93ee5617e446f3d2c7f6f63adf2e56029
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-02-10 17:01:55 +0300

    правки по слиянию

--- js/pin.js
+++ js/pin.js
@@ -8,14 +8,16 @@
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var pinFragment = document.createDocumentFragment();
 
-  var generatePinElement = function (ad, i) {
-    var pinElement = pinTemplate.cloneNode(true);
-    pinElement.style.left = ad.location.x - (AD_PIN_WIDTH / 2) + 'px';
-    pinElement.style.top = ad.location.y - AD_PIN_HEIGHT + 'px';
-    pinElement.querySelector('img').setAttribute('src', ad.author.avatar);
-    pinElement.querySelector('img').setAttribute('alt', ad.offer.title);
-    pinElement.setAttribute('data-i', i);
-    pinFragment.appendChild(pinElement);
+  var generatePinElement = function (ad) {
+    for (var i = 0; i < ad.length; i++) {
+      var pinElement = pinTemplate.cloneNode(true);
+      pinElement.style.left = ad[i].location.x - (AD_PIN_WIDTH / 2) + 'px';
+      pinElement.style.top = ad[i].location.y - AD_PIN_HEIGHT + 'px';
+      pinElement.querySelector('img').setAttribute('src', ad[i].author.avatar);
+      pinElement.querySelector('img').setAttribute('alt', ad[i].offer.title);
+      pinElement.setAttribute('data-i', i);
+      pinFragment.appendChild(pinElement);
+    }
   };
 
   window.backend.load(generatePinElement);
