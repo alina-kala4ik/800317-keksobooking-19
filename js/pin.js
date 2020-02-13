890082e29463cece36caf3fc2761a6ecc4ab44a0
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-02-13 15:01:21 +0300

    Если в объекте с описанием объявления отсутствует поле offer, то метка объявления не должна отображаться на карте.

--- js/pin.js
+++ js/pin.js
@@ -10,13 +10,15 @@
 
   var generatePinElement = function (ad) {
     for (var i = 0; i < ad.length; i++) {
-      var pinElement = pinTemplate.cloneNode(true);
-      pinElement.style.left = ad[i].location.x - (AD_PIN_WIDTH / 2) + 'px';
-      pinElement.style.top = ad[i].location.y - AD_PIN_HEIGHT + 'px';
-      pinElement.querySelector('img').setAttribute('src', ad[i].author.avatar);
-      pinElement.querySelector('img').setAttribute('alt', ad[i].offer.title);
-      pinElement.setAttribute('data-i', i);
-      pinFragment.appendChild(pinElement);
+      if (ad[i].offer !== undefined) {
+        var pinElement = pinTemplate.cloneNode(true);
+        pinElement.style.left = ad[i].location.x - (AD_PIN_WIDTH / 2) + 'px';
+        pinElement.style.top = ad[i].location.y - AD_PIN_HEIGHT + 'px';
+        pinElement.querySelector('img').setAttribute('src', ad[i].author.avatar);
+        pinElement.querySelector('img').setAttribute('alt', ad[i].offer.title);
+        pinElement.setAttribute('data-i', i);
+        pinFragment.appendChild(pinElement);
+      }
     }
   };
 
