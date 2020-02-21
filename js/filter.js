4173dfa2ff5d1480ee6af0f01058a5691c5c8071
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-02-21 10:48:08 +0300

    правки по замечаниям

---
+++ js/filter.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function () {
+
+  var QUALITY_PINS = 5;
+
+  var mapFilters = document.querySelector('.map__filters');
+  var housingType = mapFilters.querySelector('#housing-type');
+  var mapPins = document.querySelector('.map__pins');
+
+  var adsLocal = [];
+  var houseTypeChech;
+
+  var copyData = function (adsBackend) {
+    adsLocal = adsBackend;
+    filterQualityPins(adsLocal);
+  };
+
+  window.backend.load(copyData);
+
+  var filterQualityPins = function (someMassive) {
+    var qualityPinsMassive = someMassive.slice(0, QUALITY_PINS);
+    window.pin.generate(qualityPinsMassive);
+  };
+
+  var filterTypeHousing = function () {
+    if (houseTypeChech !== 'any') {
+      var typeHouseMassive = adsLocal.filter(function (element) {
+        return element.offer.type === houseTypeChech;
+      });
+      filterQualityPins(typeHouseMassive);
+    } else {
+      filterQualityPins(adsLocal);
+    }
+  };
+
+  var mapFiltersChangeHandler = function () {
+    var unnecessaryMapCard = document.querySelector('.map__card');
+    if (unnecessaryMapCard) {
+      unnecessaryMapCard.parentNode.removeChild(unnecessaryMapCard);
+    }
+  };
+
+  mapFilters.addEventListener('change', mapFiltersChangeHandler);
+
+  var updatePins = function () {
+    var allPins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+    allPins.forEach(function (item) {
+      item.parentNode.removeChild(item);
+    });
+    mapPins.appendChild(window.pin.return);
+  };
+
+  mapFilters.addEventListener('change', function (evt) {
+    if (evt.target === housingType) {
+      houseTypeChech = evt.target.value;
+      filterTypeHousing();
+      updatePins();
+    }
+  });
+
+})();
--- js/pin.js
+++ js/pin.js
@@ -33,10 +33,9 @@
     });
   };
 
-  window.backend.load(generatePinElement);
-
   window.pin = {
-    return: pinFragment
+    return: pinFragment,
+    generate: generatePinElement
   };
 
 })();
